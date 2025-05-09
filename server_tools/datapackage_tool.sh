f68f6c79f4f6d95dcacb4f194e71b82e3e42c519
ctrl-alt-tak <ctrlaltak@gmail.com>
2025-05-08 20:09:18 -0500

    fix vars

--- server_tools/datapackage_tool.sh
+++ server_tools/datapackage_tool.sh
@@ -52,8 +52,8 @@ config() {
  TRUSTSTORE_JKS=$(grep 'truststoreFile=' "/opt/tak/CoreConfig.xml" | grep -v 'fed-truststore' | sed -n 's/.*truststoreFile="\([^"]*\)".*/\1/p')
  TRUSTSTORE_BN=$(basename "/opt/tak/${TRUSTSTORE_JKS}")
  TRUSTSTORE_PEM="${TRUSTSTORE_BN%.jks}.pem"
- CA_CERT_PATH="/opt/tak/certs/files/${TRUSTSTORE_JKS%.jks}.p12"
- CACERT="${TRUSTSTORE_BN%.jks}"
+ CA_CERT_PATH="/opt/tak/certs/files/${TRUSTSTORE_BN%.jks}.p12"
+ CACERT="${TRUSTSTORE_BN%.jks}.p12"
  ls /opt/tak/certs/files | grep "admin"
  read -p "Enter webadmin cert name (.p12): " QWEBADMIN
  ADMIN="/opt/tak/certs/files/${QWEBADMIN}"
